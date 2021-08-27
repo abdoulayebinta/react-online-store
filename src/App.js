941bcf5201272ee78d411cc36b9efaccec95d22a
abdoulayebinta <bahabdoulayebinta@gmail.com>
2021-08-27 23:20:03 +0000

    broken homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import HomePage from "./components/HomePage";
+import HomePage from "./pages/homepage/HomePage";
 
 function App() {
   return (
---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import MenuItem from "../menu-item/MenuItem";
+
+import "./Directory.scss";
+
+const Directory = () => {
+  const sectionsData = [
+    {
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      id: 1,
+      linkUrl: "shop/hats",
+    },
+    {
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      id: 2,
+      linkUrl: "shop/jackets",
+    },
+    {
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      id: 3,
+      linkUrl: "shop/sneakers",
+    },
+    {
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      size: "large",
+      id: 4,
+      linkUrl: "shop/womens",
+    },
+    {
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      size: "large",
+      id: 5,
+      linkUrl: "shop/mens",
+    },
+  ];
+
+  const [sections, setSections] = useState(sectionsData);
+
+  return (
+    <div className="directory-menu">
+      {sections.map(({ id, title, imageUrl, size }) => (
+        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
